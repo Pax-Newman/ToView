68ee74eec1db9a61abdba861eafa6ed7bc7a4ac9
Pax-Newman <newmanp@wwu.edu>
2022-11-16 13:33:01 -0800

    renamed package to be lowercase

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2022 Pax Newman <pax.newman@gmail.com>
 */
 package main
 
-import "github.com/Pax-Newman/ToView/cmd"
+import "github.com/Pax-Newman/toview/cmd"
 
 func main() {
 	cmd.Execute()
