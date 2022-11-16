a019b31153fef4e22ea7e955d63c7784a65aa7d2
Pax-Newman <newmanp@wwu.edu>
2022-11-16 13:34:42 -0800

    implemented basic parsing of multiple files

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,12 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/Pax-Newman/toview/parse"
+	"github.com/charmbracelet/glamour"
 	"github.com/spf13/cobra"
 )
 
@@ -19,9 +23,66 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+
+	// TODO write custom validator to accept any number of valid file paths
+	// TODO add --ignore-unsupported flag to skip unsupported filetypes
+	Args: cobra.ArbitraryArgs,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(args)
+
+		datas := []parse.FileData{}
+		// parse data for each file in args
+		for _, path := range args {
+			data, err := parse.LineByLine(path)
+			if err != nil {
+				cobra.CompError(err.Error())
+			}
+			datas = append(datas, data)
+		}
+
+		// init the string that we will render and display
+		renderStr := ""
+
+		// prepare data from each file for the render
+		for _, data := range datas {
+
+			// report the filename
+			// TODO consider if there should be a config for reporting the relative path instead
+			renderStr += fmt.Sprintf("# %s\n", filepath.Base(data.FilePath))
+
+			// check if there's anything to report in the file
+			if len(data.ToDo) <= 0 && len(data.FixMe) <= 0 {
+				renderStr += "### No comments available to report\n"
+				continue
+			}
+
+			// check for and display TODOs in the file
+			renderStr += "## To Do\n"
+			if len(data.ToDo) > 0 {
+				for _, todo := range data.ToDo {
+					renderStr += fmt.Sprintf(" - __%d:__ %s\n", todo.Position, todo.Content)
+				}
+			} else {
+				renderStr += "### No ToDos to report\n"
+			}
+
+			// check for and display FIXMEs in the file
+			renderStr += "## Fix Me\n"
+			if len(data.FixMe) > 0 {
+				for _, fixme := range data.FixMe {
+					renderStr += fmt.Sprintf(" - __%d:__ %s\n", fixme.Position, fixme.Content)
+				}
+			} else {
+				renderStr += "### No FixMes to report\n"
+			}
+		}
+
+		// FIXME handle the err from the render method
+		// TODO allow users to set their own custom style in a config
+		out, _ := glamour.Render(renderStr, "dark")
+		fmt.Println(out)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -43,4 +104,6 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// TODO add flag for "quiet output" i.e only show true output, no loading bars
 }
