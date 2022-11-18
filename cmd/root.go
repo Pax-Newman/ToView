a5d25cf76aafad1d7aea8f783333769602749a78
Pax-Newman <newmanp@wwu.edu>
2022-11-17 16:01:23 -0800

    moved renderCategory to render package

--- cmd/root.go
+++ cmd/root.go
@@ -10,33 +10,10 @@ import (
 	"reflect"
 
 	"github.com/Pax-Newman/toview/parse"
+	"github.com/Pax-Newman/toview/render"
 	"github.com/charmbracelet/glamour"
 	"github.com/spf13/cobra"
 )
-
-func renderCategory(cmd *cobra.Command, category string, items []parse.Comment) string {
-	// get flags
-	flagAll, _ := cmd.Flags().GetBool("all")
-
-	// init the string we'll build and return
-	renderStr := ""
-
-	// check if category is empty
-	if len(items) <= 0 {
-		if flagAll {
-			// report anyways if --all is set
-			renderStr += fmt.Sprintf("## %s\n", category)
-			renderStr += "### No comments to report\n"
-		}
-		return renderStr
-	}
-	// if there are items then populate our render string with them
-	renderStr += fmt.Sprintf("## %s\n", category)
-	for _, item := range items {
-		renderStr += fmt.Sprintf(" - __%d:__ %s\n", item.Position, item.Content)
-	}
-	return renderStr
-}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -133,7 +110,7 @@ var rootCmd = &cobra.Command{
 			renderStr += fmt.Sprintf("# %s\n", filepath.Base(data.FilePath))
 
 			for _, category := range data.Categories {
-				renderStr += renderCategory(cmd, category.Name, category.Comments)
+				renderStr += render.RenderCategory(cmd, category.Name, category.Comments)
 			}
 		}
 
